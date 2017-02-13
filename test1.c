51c28e4e7dcbe5a16c995aac1cff9381c137328c
Shafiur Rahman <shafeeyrahman@gmail.com>
2017-02-13 12:36:36 -0800

    stride

---
+++ test1.c
@@ -0,0 +1,21 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+  int num_tickets = 30;
+  printf(1, "Started process id %d with %d tickets\n", getpid(), num_tickets);
+
+  settickets(num_tickets);
+  int i,k;
+  const int loop=43000;
+
+  for(i=0;i<loop;i++)
+    for(k=0;k<loop;k++)
+        asm("nop");
+
+  getstat();
+
+  exit();
+}
